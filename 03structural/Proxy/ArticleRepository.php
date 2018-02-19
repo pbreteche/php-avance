26b6b346ef429b5b9f7c849c52cba703bc2505cb
Pierre Bretéché <pbreteche@dawan.fr>
2018-02-19 17:33:37 +0100

    proxy

---
+++ 03structural/Proxy/ArticleRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace structural\Proxy;
+
+
+class ArticleRepository
+{
+    public function getArticle()
+    {
+        sleep(4);
+        return ['article premier', 'article second'];
+    }
+}
---
+++ 03structural/Proxy/main.php
@@ -0,0 +1,18 @@
+<?php
+namespace structural\Proxy;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+$proxy = new RepositoryProxy('article');
+
+echo (new \DateTime())->format('c') . "\n";
+$proxy->getList();
+echo (new \DateTime())->format('c') . "\n";
+$proxy->getList();
+echo (new \DateTime())->format('c') . "\n";
+$proxy->getList();
+echo (new \DateTime())->format('c') . "\n";
+$proxy->getList();
+echo (new \DateTime())->format('c') . "\n";
+$proxy->getList();
+echo (new \DateTime())->format('c') . "\n";
